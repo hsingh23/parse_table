2f916571063a5057f0b260ea88b06eba68d714ae
Philip Hann <phann92@gmail.com>
2013-09-04 19:35:25 -0500

    column major

--- parse_table.py
+++ parse_table.py
@@ -1,9 +1,9 @@
+
 from string import split
 
 class Table:
     def __init__(self):
-        self.labels = []
-        self.data = []
+        self.data = {}
 
     def populate(self, file_name, col_delim="\t", row_delim="\n", data_start=None):
         with open(file_name, 'r') as f:
@@ -14,33 +14,41 @@ class Table:
                         break
             raw_data = f.read()
         rows = [split(raw_row, col_delim) for raw_row in split(raw_data, row_delim)]
-        self.data = [map(float, row) for row in rows[1:]]
-        self.labels = {label: index for index, label in enumerate(rows[0])}
+        parse_col = lambda index : self._parse_col(rows[1:], index, float)
+        self.data = {
+            label : parse_col(index) for index, label in enumerate(rows[0])
+        }
 
-    def get_label_ind(self, label):
-        if label not in self.labels:
-            raise KeyError('No column with label : "' + label + '".')
-        return self.labels[label]
+    def _parse_col(self, rows, col_ind, conv=None):
+        if not conv:
+            return [row[col_ind] for row in rows]
+        try:
+            return [conv(row[col_ind]) for row in rows]
+        except ValueError:
+            return self._parse_col(rows, col_ind, None)
+        except IndexError:
+            raise IndexError('A data element is missing.')
 
     def get_column(self, data_label):
-        col_ind = self.get_label_ind(data_label)
-        return [data_row[col_ind] for data_row in self.data]
+        return self.data[data_label]
 
     # returns dict of format {class_id : { label : data_column}}
     def get_classified_columns(self, classifier, class_label, *labels):
         if len(labels) is 0:
             labels = [class_label]
         classes = {}
-        c_ind = self.get_label_ind(class_label)
-        for row in self.data:
-            class_id = row[c_ind] if classifier is None else classifier(row[c_ind])
+        class_col = self.get_column(class_label)
+        for row_ind in range(len(class_col)):
+            class_id = class_col[row_ind]
+            if classifier is not None:
+                class_id = classifier(class_id)
             for label in labels:
-                d_ind = self.get_label_ind(label)
+                col = self.data[label]
                 if class_id not in classes:
                     classes[class_id] = {}
                 if label not in classes[class_id]:
                     classes[class_id][label] = []
-                classes[class_id][label].append(row[d_ind])
+                classes[class_id][label].append(col[row_ind])
         return classes
 
     def map_columns(self, fn, *labels):
